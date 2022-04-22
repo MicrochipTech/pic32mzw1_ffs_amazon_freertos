5e310eaf6a923e1740f43aed51e88ab610ee7ed7
Hemanth Tenkod <hemanth.tenkod@microchip.com>
2022-04-21 20:16:04 -0700

    Generates the devPubkey, devTypePubKey and RootCA files to store in WFI32-IoT MSD

---
+++ tools/create-ffs-msd-files.py
@@ -0,0 +1,68 @@
+from os import pipe, write
+import sys, getopt
+from  cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography import x509
+
+def main(argv):
+	ffsRootCa = 'SRootCA.cer'   	
+	ffsDevTypePubKey = 'device-type-public.key'
+	ffsDevCert = 'device-certificate.pem'	
+	try:
+		opts, args = getopt.getopt(argv,"hr:c:k:t:",["ca=","cert=","key=","type="])
+	except getopt.GetoptError:
+		print('create-ffs-credentials.py -r <root CA> -c <device certificate> -t <device type public key>')
+		sys.exit(2)
+	
+	for opt, arg in opts:
+		if opt == '-h':
+			print('create-ffs-credentials.py -r <root CA> -c <device certificate> -t <device type public key>')
+			sys.exit()
+		elif opt in ("-r", "--ca"):
+			ffsRootCa = arg
+		elif opt in ("-c", "--cert"):		
+			ffsDevCert = arg
+		elif opt in ("-t", "--type"):		  
+			ffsDevTypePubKey = arg
+	print('Root CA file is ', ffsRootCa)
+	print('Device Certificate file is ', ffsDevCert)	
+	print('Device Type Public file is ', ffsDevTypePubKey)
+	print("-------Root CA-------")
+	##Amazon FFS DSS CA
+	with open(ffsRootCa, 'rb') as fHdl:
+		try:
+			certHdl = x509.load_der_x509_certificate(fHdl.read())
+			caCertArray = certHdl.public_bytes(serialization.Encoding.DER)
+			with open("../ffsRootCa.der", 'wb') as file:
+				file.write(caCertArray)				
+		except Exception as e:
+			print(e)
+
+
+	print("-------Pub Key-------")
+	with open(ffsDevCert, 'rb') as fHdl:
+		try:
+			certHdl = x509.load_pem_x509_certificate(fHdl.read())
+			pubKeyHdl = certHdl.public_key()			
+			pubKeyBytes = pubKeyHdl.public_bytes(serialization.Encoding.DER,
+			serialization.PublicFormat.SubjectPublicKeyInfo)			
+			with open("../ffsDevPublic.key", 'wb') as file:
+				file.write(pubKeyBytes)	
+		except Exception as e:
+			print(e)			
+	
+	print("-------Dev Type Pub Key-------")
+	##Device Type public key
+	with open(ffsDevTypePubKey, 'rb') as fHdl:
+		try:
+			devTypePubKeyHdl = serialization.load_pem_public_key(fHdl.read(), default_backend())
+			devTypePubKeyBytes = devTypePubKeyHdl.public_bytes(serialization.Encoding.DER, 
+			serialization.PublicFormat.SubjectPublicKeyInfo)
+			with open("../ffsDevTypePublic.key", 'wb') as file:
+				file.write(devTypePubKeyBytes)	
+		except Exception as e:
+			print(e)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
